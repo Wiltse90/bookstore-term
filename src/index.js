bcca91f545ce46b92af5881a21bf28a0c680cd5b
Steven <Wiltse90@gmail.com>
2022-03-28 02:32:12 -0500

    Package Update

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Login from './components/login/Login';
+import Signup from './components/signup/Signup';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { render } from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Signup from './components/signup/Signup';
-import Login from './components/login/Login';
 
-const rootElement = document.getElementById("root");
-render(
-  <BrowserRouter>
+ReactDOM.render(
+  <React.StrictMode>
+      <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/signup/Signup" element={<Signup />} />
       <Route path="/login/Login" element={<Login />} />
     </Routes>
   </BrowserRouter>,
-  rootElement);
-
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
