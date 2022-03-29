0a965feb79ec8ce146234e11101866d71dc60150
Steven <Wiltse90@gmail.com>
2022-03-28 22:03:05 -0500

    Update Ui

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Form from './components/form/Form';
 import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 function App() {
   return (
     <div className='App'>
-    <Header />
+    <Header/>
     <Form/>
+    <Footer/>
     </div>
   );
 }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './style.css';
+
+
+const Footer = () =>{
+    return(
+        <footer className="footer">
+            <p>Steven Wiltse - Term Project</p>
+        </footer>
+    )
+}
+
+export default Footer;
