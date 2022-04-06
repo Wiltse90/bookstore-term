e09f887ff91a515b99ab52b6896aa9ff968f36de
Steven <Wiltse90@gmail.com>
2022-04-05 23:12:08 -0500

    Week 3

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,24 +1,39 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import React from "react";
 import Login from "../login/Login";
 import Signup from "../signup/Signup";
 import './style.css';
 
+
 const Form = () => {
-    const btnLog = <button type="button" onClick={() => {setAuth(Login); setBtn(btnSign); setMsg("Not a member? Register here.");}}>Login</button>;
-    const btnSign = <button type="button" onClick={() => {setAuth(Signup); setBtn(btnLog); setMsg("Already a member? Log in here.");}}>Register</button>;
+    
+    const [user, setUser] = useState(
+        {
+            uname:"",
+            pwd:"",
+            fName:"",
+            lName:""
+        }
+    );
 
-    const [userAuth, setAuth] = useState(Login);
-    const [btnAuth, setBtn] = useState(btnSign);
-    const [msg, setMsg] = useState("Not a member? Register here.");
 
+    const callback = useCallback((user) => {
+        setRegistered(registered => !registered);
+        event.preventDefault();
+        setUser(user);
+    }, []);
+
+    const [registered, setRegistered] = useState(true);
+    const [frmAuth, setAuth] = useState(<Login parentCallback={callback} user={user}/>);
+    
+    const getForm = () => {
+        return registered ? <Login parentCallback={callback} user={user}/> : <Signup parentCallback={callback} user={user}/>;
+    }
     return (   
 
-        <div className="authContainer">
-        {userAuth}
+        <div id="authContainer">
 
-        <div className="authMsg">{msg} {btnAuth}</div>
-        
+            {getForm()}
         </div>
     )
 }
